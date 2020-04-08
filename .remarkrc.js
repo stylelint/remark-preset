ac0db46b2b14bd531d5baa07ea7ddac81d35c08c
jeddy3 <jeddy3@users.noreply.github.com>
2020-04-08 10:23:04 +0100

    Update deps and prepare release

---
+++ .remarkrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+	plugins: [
+		require('remark-frontmatter'),
+		require('remark-preset-lint-recommended'),
+		require('remark-validate-links'),
+		require('remark-preset-prettier'),
+	],
+};
--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-"use strict";
+'use strict';
 
-module.exports = {
-  plugins: [
-    require("remark-frontmatter"),
-    require("remark-preset-lint-recommended"),
-    require("remark-validate-links"),
-    require("remark-preset-prettier"),
-  ],
-};
+module.exports = require('./.remarkrc.js');
